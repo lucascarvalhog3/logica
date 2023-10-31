cb2a2d4e6298eec47b3fc7cf8b0b33597a8f3bca
lucascarvalhog3 <147560956+lucascarvalhog3@users.noreply.github.com>
2023-10-30 21:52:35 -0300

    Add files via upload

---
+++ prova algoritmos  e variaveis/prova.py
@@ -0,0 +1,8 @@
+nome = "lucas" # tipo string (str)
+idade = 29 # tipo inteiro (int)
+altura = 1,29 #tipo decimal (float)
+matriculado = True #tipo boleano (bool)
+
+
+nome = str(input("qual é o seu nome? "))
+print(f'olá {nome}, seja bem vindo!')
